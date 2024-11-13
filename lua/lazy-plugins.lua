4619e6bfb1dc815ecd07fce7d50fbd1d25aec733
David Wright <david.wright@globalrelay.net>
2024-11-13 10:44:41 -0800

    oil

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -45,6 +45,7 @@ require('lazy').setup({
   require 'kickstart/plugins/treesitter',
   require 'kickstart/plugins/lazygit',
   require 'kickstart/plugins/harpoon',
+  require 'kickstart/plugins/oil',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
